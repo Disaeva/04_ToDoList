cf4fd4dff8f14cfd581e2aa3523eb1bb662b8238
Disaeva <isaevado@gmail.com>
2023-10-06 10:32:31 -0400

    final commit

--- script_ToDoList.js
+++ script_ToDoList.js
@@ -10,14 +10,25 @@ let count = 0
 function addToList() {
     let myTaskList = document.getElementById('taskList')
     let myInput = document.getElementById('task')
+    let taskText = myInput.value.trim(); // Trim whitespace from the input value
 
-    count++
+    if (taskText !== "") { // Check if the input is not empty
+        count++;
 
-    if (count <= 5) {
-        myTaskList.innerHTML = myTaskList.innerHTML + '<p>' + count + ". " + myInput.value + '</p>'
-    } else {
-        alert ("Не берите на себя слишком много!")
+        if (count <= 5) {
+            myTaskList.innerHTML = myTaskList.innerHTML + '<p>' + count + ". " + myInput.value + '</p>'
+            myInput.value = ''
+        } else {
+            alert ("Не берите на себя слишком много!")
     }
-       
+}
 
 }
+
+// Add an event listener to the input field for the "Enter" key press
+let taskInput = document.getElementById('task');
+taskInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        addToList(); // Call addToList when Enter is pressed
+    }
+});
